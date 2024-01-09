06b5c46884c4121c56894ed5cec9902d828b93b1
matheusyanmonteiro <matheusyanmonteiro@gmail.com>
2024-01-09 17:48:08 -0300

    feat: create the foundation of domain for forum aplication

---
+++ src/domain/use-cases/answer-question.spec.ts
@@ -0,0 +1,22 @@
+import { AnswerQuestionUseCase } from './answer-question';
+import { AnswersRepository } from '../repositories/answers-repository';
+import { Answer } from '../entities/answer';
+
+const fakeAnswersRepository: AnswersRepository = {
+  create:  async function (answer: Answer): Promise<void> {
+    return;
+  }
+}
+
+
+test('create an answer ', async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
+
+  const answer = await answerQuestion.execute({
+    questionId: '1',
+    instructorId: '1',
+    content: 'New answer.'
+  })
+
+  expect(answer.content).toEqual('New answer.')
+});
