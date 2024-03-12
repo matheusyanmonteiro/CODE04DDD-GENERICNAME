ce6bb9bf386917d4afd654752fc1dea1155c6ac1
matheusyanmonteiro <matheusyanmonteiro@gmail.com>
2024-03-12 15:09:51 -0300

    feat: create the watched list to suport the changes on cascate

--- src/domain/forum/application/use-cases/create-question.spec.ts
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -1,5 +1,6 @@
 import { InMemoryQuestionsRepository } from 'test/repository/in-memory-questions-repository'
 import { CreateQuestionUseCase } from './create-question'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
 let inMemoryQuestionRepository: InMemoryQuestionsRepository
 let sut: CreateQuestionUseCase
@@ -15,9 +16,19 @@ describe('Create Question', () => {
       authorId: '1',
       title: 'New Question',
       content: 'content question.',
+      attachmentsIds: ['1', '2'],
     })
 
     expect(result.isRight())
     expect(inMemoryQuestionRepository.items[0]).toEqual(result.value?.question)
+    expect(
+      inMemoryQuestionRepository.items[0].attachments.currentItems,
+    ).toHaveLength(2)
+    expect(
+      inMemoryQuestionRepository.items[0].attachments.currentItems,
+    ).toEqual([
+      expect.objectContaining({ attachmentId: new UniqueEntityID('1') }),
+      expect.objectContaining({ attachmentId: new UniqueEntityID('2') }),
+    ])
   })
 })
