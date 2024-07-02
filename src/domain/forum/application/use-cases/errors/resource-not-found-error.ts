27765ce69ced3a279b3db947fceb0541b6206a4c
matheusyanmonteiro <matheusyanmonteiro@gmail.com>
2024-07-02 20:30:44 -0300

    fix: eslint

--- src/domain/forum/application/use-cases/errors/resource-not-found-error.ts
+++ src/domain/forum/application/use-cases/errors/resource-not-found-error.ts
@@ -1,6 +1,7 @@
 import { UseCaseError } from '@/core/errors/use-case-error'
 
 export class ResourceNotFoundError extends Error implements UseCaseError {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   question: any
   constructor() {
     super('Resource not found')
