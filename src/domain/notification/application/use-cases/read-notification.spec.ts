e6e67ec3f123b1dcae21dc7d6b44cd996ecb0b34
matheusyanmonteiro <matheusyanmonteiro@gmail.com>
2024-07-05 16:37:30 -0300

    feat: add the test for prevents if the outher user try to read a notification

--- src/domain/notification/application/use-cases/read-notification.spec.ts
+++ src/domain/notification/application/use-cases/read-notification.spec.ts
@@ -1,3 +1,5 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { NotAllowedError } from '@/core/errors/errors/not-allowed-error'
 import { makeNotification } from 'test/factory/make-notification'
 import { InMemoryNotificationsRepository } from 'test/repository/in-memory-notifications-repository'
 import { ReadNotificationUseCase } from './read-notification'
@@ -26,5 +28,21 @@ describe('Read Notification', () => {
     expect(inMemoryNotificationRepository.items[0].readAt).toEqual(
       expect.any(Date),
     )
+  })
+
+  it('should not be able to read a notification from another user', async () => {
+    const notification = makeNotification({
+      recipientId: new UniqueEntityID('recipent-1'),
+    })
+
+    await inMemoryNotificationRepository.create(notification)
+
+    const result = await sut.execute({
+      notificationId: notification.id.toString(),
+      recipentId: 'recipent-2',
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(NotAllowedError)
   })
 })
