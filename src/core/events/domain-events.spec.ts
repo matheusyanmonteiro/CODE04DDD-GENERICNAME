064fdb39ec1ae3a6efe908ec0dbd5e5633bc162e
matheusyanmonteiro <matheusyanmonteiro@gmail.com>
2024-07-11 17:51:45 -0300

    feat: create test for domainEvent process

--- src/core/events/domain-events.spec.ts
+++ src/core/events/domain-events.spec.ts
@@ -1,5 +1,48 @@
-describe('DomainEvent', () => {
-  it('should create a new domain event', () => {
-    // TODO: Write your test code here
+import { vi } from 'vitest'
+import { AggregateRoot } from '../entities/aggregate-root'
+import { UniqueEntityID } from '../entities/unique-entity-id'
+import { DomainEvent } from './domain-event'
+import { DomainEvents } from './domain-events'
+
+class CustomAggregateCreated implements DomainEvent {
+  public ocurredAt: Date
+  // eslint-disable-next-line no-use-before-define
+  private aggregate: CustomAggregate // eslit-disable-line
+
+  constructor(aggregate: CustomAggregate) {
+    this.aggregate = aggregate
+    this.ocurredAt = new Date()
+  }
+
+  public getAggregateId(): UniqueEntityID {
+    return this.aggregate.id
+  }
+}
+
+class CustomAggregate extends AggregateRoot<null> {
+  static create() {
+    const aggregate = new CustomAggregate(null)
+
+    aggregate.addDomainEvent(new CustomAggregateCreated(aggregate))
+
+    return aggregate
+  }
+}
+
+describe('domain events', () => {
+  it('should be able to dispatch and listen to events', async () => {
+    const callbackSpy = vi.fn()
+
+    DomainEvents.register(callbackSpy, CustomAggregateCreated.name)
+
+    const aggregate = CustomAggregate.create()
+
+    expect(aggregate.domainEvents).toHaveLength(1)
+
+    DomainEvents.dispatchEventsForAggregate(aggregate.id)
+
+    expect(callbackSpy).toHaveBeenCalled()
+
+    expect(aggregate.domainEvents).toHaveLength(0)
   })
 })
